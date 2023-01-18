d6dfdcd5b85f8117bc76b38340f867427a717a2f
princejohnson37 <prince@email.com>
2023-01-19 01:19:08 +0530

    initial commit

---
+++ src/components/part1/Part1.jsx
@@ -0,0 +1,26 @@
+import { Box, Grid, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+import React from "react";
+
+function Part1() {
+  return (
+    <Grid container>
+      <Grid item xs={6}>
+        <Box className="bg-color-box">
+          <Container>
+            <Typography>Constructing Spaces For You</Typography>
+            <Typography>
+              We provide all kind of construction services with advanced
+              technology and precise quality.
+            </Typography>
+          </Container>
+        </Box>
+      </Grid>
+      <Grid item xs={6}>
+        <Box className="bg-img-box"></Box>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Part1;
